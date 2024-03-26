d3cb8426ce6f498f2d827bf14895dda1caf46847
Yuvraj <yuvrajsingh.3402@gmail.com>
2024-03-26 21:20:40 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-##TO SETUP THE FILE USE "python setup.py install" in the terminal
+from setuptools import setup,find_packages
+from typing import List
 
-
-from setuptools import setup, find_packages
-
-
-
+#Declaring variables for setup functions
 PROJECT_NAME="tsunami_prediction"
 VERSION="0.0.1"
 AUTHOR="yuvraj singh"
 DESCRIPTION="This project helps predict tsunami occurences"
-PACKAGES=["tsunami"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
-def get_requirements_lists():
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
+
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e .")
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
 
 
 setup(
-    name=PROJECT_NAME,
-    version=VERSION,
-    author=AUTHOR,
-    description=DESCRIPTION,
-    packages=find_packages(),
-    install_requires=get_requirements_lists()
-    )
-   
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
+)
